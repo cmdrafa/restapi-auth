5da03ab02668712bf327e828607a68b97fdf07fb
Rafael Kraemer <rkraemer01@gmail.com>
2017-04-11 12:02:37 +0100

    New routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ var actions = require('../methods/actions');
 
 var router = express.Router();
 
-router.post('/authenticate', actions.authenticate);
-router.post('/adduser', actions.addNew);
-router.get('/getinfo', actions.getinfo);
+router.post('authapi/authenticate', actions.authenticate);
+router.post('authapi/adduser', actions.addNew);
+router.get('authapi/getinfo', actions.getinfo);
 
 module.exports = router;
