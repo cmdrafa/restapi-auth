2b51a1bd4f1bfcc897c650bede09c90d548042db
Rafael Kraemer <rkraemer01@gmail.com>
2017-04-20 13:20:52 +0100

    Still broken

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,10 @@
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 //Models an config files
 var User = require('../model/user.js');
 var config = require('../config/database');
-var googleAuth = require('../config/auth');
 
 
 module.exports = function(passport) {
@@ -28,82 +26,4 @@ module.exports = function(passport) {
             }
         })
     }));
-
- /*   passport.use(new GoogleStrategy({
-
-        clientID : googleAuth.googleAuth.clientID,
-        clienSecret : googleAuth.googleAuth.clientSecret,
-        callbackURL : googleAuth.googleAuth.callbackURL,
-        passReqToCallback : true
-        
-    },
-    function(req, token, refreshToken, profile, done) {
-
-        process.nextTick(function() {
-
-            if(!req.user) {
-                User.findOne({ 'google.id': profile.id}, function(err, user){
-                    if(err){
-                        return done(err);
-                    }
-
-                    if(user){
-                        if(!user.google.token) {
-                            user.google.token = token;
-                            user.google.name = profile.displayName;
-                            user.google.email = (profile.emails[0].value || '').toLowerCase();
-
-                            user.save(function(err){
-                                if(err){
-                                    return done(err);
-                                }
-                                else{
-                                    var token = jwt.encode(user, config.secret);
-                                    res.json({success: true, token: token});
-                                    return done(null, user);
-                                }
-                            });
-                        }
-                        return done(null, user);
-                    } else {
-                        var newUser = new User();
-
-                        newUser.google.id = profile.id;
-                        newUser.google.token = token;
-                        newUser.google.name = profile.displayName;
-                        newUser.google.email = (profile.emails[0].value || '').toLowerCase();
-
-                        newUser.save(function(err) {
-                            if(err){
-                                return done(err);
-                            }
-                            else{
-                                var token = jwt.encode(newUser, config.secret);
-                                res.json({success: true, token: token});
-                                return done(null, newUser);
-                            }
-                        })
-                    }
-
-                })
-            } else {
-                var user = req.user 
-
-                user.google.id = profile.id;
-                user.google.toLowerCase = token;
-                user.google.name = profile.displayName;
-                user.google.email = (pofile.emails[0].value || '').toLowerCase();
-
-                user.save(function(err) {
-                    if(err){
-                        return done(err);
-                    }
-                    else{
-                        return done(null, user);
-                    }
-                });
-            }
-
-        });
-    }));*/
 };
