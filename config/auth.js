cd91e40433d296e7da4712202790b26e483cd4f5
Rafael Kraemer <rkraemer01@gmail.com>
2017-04-20 21:11:29 +0100

    Same as last one

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
         'googleAuth' : {
         'clientID'         : '282554064261-t0e4ft8a7v2lh8tvdo06h32qrh4m8pu9.apps.googleusercontent.com',
         'clientSecret'     : 'WBS8l2IB08vqazGD2mv5kaV1',
-        'callbackURL'      : 'https://rafakr.cf/authapi/google/callback'
+        'callbackURL'      : 'https://rafakr.cf/authapi/auth/google/callback'
     }
 
 }
